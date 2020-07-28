5269a4be3e415540449370d6fbd7c75fe0fd230d
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2020-07-28 16:23:22 +0600

    Added hello-kube project

---
+++ hello-kube/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/kubernetes-handbook-github.webp" />
+  <HelloKube msg="The Kubernetes Handbook" />
+</template>
+
+<script>
+import HelloKube from './components/HelloKube.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloKube
+  }
+}
+</script>
---
+++ hello-kube/src/components/HelloKube.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>A folow-up to <a href="https://www.freecodecamp.org/news/the-docker-handbook/" target="_blank">The Docker Handbook</a></p>
+  <hr>
+  <p>
+    <strong>Author:</strong> <a href="https://www.freecodecamp.org/news/author/farhanhasin/" target="_blank">Farhan Hasin Chowdhury</a>
+    <br>
+    <strong>Website:</strong> <a href="https://www.farhan.info/" target="_blank">https://farhan.info/</a>
+    <br>
+    <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/farhanhasin/" target="_blank">/in/farhanhasin</a>
+    <br>
+    <strong>Twitter:</strong> <a href="https://twitter.com/frhnhsin" target="_blank">@frhnhsin</a>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'HelloKube',
+  props: {
+    msg: String
+  },
+}
+</script>
